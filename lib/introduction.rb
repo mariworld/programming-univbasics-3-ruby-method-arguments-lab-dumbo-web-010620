f2a8d59a93bbc2b1791b65d1927e44ffbb7f618b
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-27 16:24:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -21,7 +21,6 @@ end
 #puts ->"Hi (name), I'm learning (language)"
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
-  
 end
 
 
